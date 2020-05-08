81ee33a2db36ac7143b688837f8b0258eef4a0a6
Karlos97 <k.minta97@gmail.com>
2020-05-08 11:08:38 +0200

    working with item delete

--- src/JS/main.js
+++ src/JS/main.js
@@ -13,7 +13,22 @@ objects.inputVal.addEventListener("keyup", function (event) {
     input();
   }
 });
-if((objects.x.addEventListener("onclick"))&&(objects.item.addEventListener("mouseover"))){console.log(`najechales na oba elementy`);}
+
+// Array.from(objects.item).forEach((el) => {
+//   el.addEventListener("mouseover", function () {
+//     console.log(`najechales na element ${el.id}`);
+//   });
+// });
+Array.from(objects.x).forEach((el1) => {
+  el1.addEventListener("click", function () {
+    console.log(`kliknales na element ${el1.parentElement.id}`);
+  });
+});
+// Array.from(objects.x).forEach((el1) => {
+//   el1.addEventListener("click", function () {
+//     console.log(`kliknales na element ${el1.parentElement.id}`);
+//   });
+// });
 
 
 // let elem = new list("abc", "efg", 3);
