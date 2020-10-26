8af42b2c640c54c94bcdd8099dc27f054cbef76c
Default Profile <pomaranczowy.orzel12@gmail.com>
2020-10-26 20:34:46 +0100

    better view

--- src/JS/models/objects.js
+++ src/JS/models/objects.js
@@ -3,7 +3,7 @@ export const objects = {
   inputVal: document.getElementById("inputVal"),
   item: document.getElementsByClassName("expenses__item"),
   itemDescr: document.getElementsByClassName("expenses__item__description"),
-  itemVal: document.getElementsByClassName("expenses__item__value"),
+  itemVal: document.getElementsByClassName("expenses__input__item--value"),
   x: document.getElementsByClassName("expenses__item__delete"),
   list: document.getElementsByClassName("expenses__list"),
   warning: document.getElementById("expenses__input__warning"),
