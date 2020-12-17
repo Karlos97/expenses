564c00c89cdf728ad46421d8ba0fbf939c76e07d
Default Profile <pomaranczowy.orzel12@gmail.com>
2020-12-17 16:48:34 +0100

    small build and fix

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,18 +1,23 @@
 import userExpensesInput from "./models/userExpensesInput.js";
 import { objects } from "./models/objects.js";
-import {cleanInput} from "./models/functions.js"
+import { cleanInput, init, storeData, deleteCookie, clearData, totalValue } from "./models/functions.js";
+import list from "./views/list.js";
+import { chart } from "./views/chart.js";
+
+let cookieList = init();
+// console.log(cookieList)
 
 objects.inputDescr.addEventListener("keyup", function (onEnterEventHandler) {
   if (onEnterEventHandler.key === "Enter") {
     for (let i = 0; i < objects.item.length; i++) {
-      
       if (objects.item[i].id !== `exp-${i}`) {
         objects.item[i].id = `exp-${i}`;
       }
       objects.id = i + 1;
     }
     userExpensesInput();
-    cleanInput()
+    cleanInput();
+    objects.historyInput.value = "Now";
   }
 });
 
@@ -25,11 +30,105 @@ objects.inputVal.addEventListener("keyup", function (onEnterEventHandler) {
       objects.id = i + 1;
     }
     userExpensesInput();
-    cleanInput()
+    cleanInput();
+    objects.historyInput.value = "Now";
   }
 });
-// after creating new list (making new id) 
+
+objects.historyInput.addEventListener("click", function () {
+  objects.historyInput.value = "";
+});
+
+
+
+objects.historyInput.addEventListener("change", function () {
+  clearData(objects.item,chart)
+  cookieList.forEach((el) => {
+    if (el[0] === this.value) {
+
+      let expensesList = el[1][0];
+      for (let i = 0; i < expensesList.length; i++) {
+        let readList = new list(expensesList[i][0], expensesList[i][1], i);
+        readList.addingNewItem();
+      }
+    }
+  });
+});
+
+objects.btnDelete.addEventListener("click", function () {
+  deleteCookie(objects.historyInput.value)
+  clearData(objects.item,chart)
+  totalValue()
+  location.reload();
+  // window.location.reload(true)
+  // window.location.reload(false); 
+});
+
+objects.btnSave.addEventListener("click", function () {
+  if (objects.item.length) {
+    let now = new Date().toString().split(" ").splice(1, 4).join("-");
+    let length = objects.item.length;
+    let list = [];
+    let sameList = false;
+    for (let i = 0; i < length; i++) {
+      list[i] = [
+        objects.item[i].childNodes[1].textContent,
+        objects.item[i].childNodes[2].textContent,
+      ];
+    }
+// cookieList.forEach( el=>{
+//   if(el[1][0] = list){
+//     console.log(`lista ${el[1][0]} jest taka sama jak lista ${list}`)
+//     sameList = true;
+//   }else sameList = false;
+// })
+// if(!sameList){
+    objects.historyList.innerHTML += `<option value=${now} />`;
+    objects.historyInput.value = "Now";
+    storeData(list, now, 365);
+    sameList = false;
+    location.reload();
+
+}
+  // }
+
+});
+
+//now:
+// choosing we delete list and prepare like for init
+// now shows up after typing new expenses when we have not opened history log
+
+//after adding new element to the list history change on "now"
+// after creating new list (making new id)
 // create save option, change history - add option NOW
 // if we already opened a save from history, we can overwrite it by pressing save
 // we can delete everything
 // choosing date from history will load expenses from that time
+
+// let date = new Date(Date.now() + 86400e3);
+// date = date.toUTCString();
+// document.cookie = "user=John; expires=" + date dodać 12 miesiecy;
+// document.cookie = "user=John; max-age=3600"; mozna tutaj w sekundachw rzucic 12 miesiecy
+
+//READ
+
+// function showCookie(name) {
+//   if (document.cookie !== "") {
+//       const cookies = document.cookie.split(/; */);
+
+//       for (let i=0; i<cookies.length; i++) {
+//           const cookiePart = cookies[i].split("=");
+//           const cookieName = cookiesPart[0];
+//           const cookieVal = cookiesPart[1];
+//           if (cookieName === decodeURIComponent(name)) {
+//               return decodeURIComponent(cookieVal);
+//           }
+//       }
+//   }
+//   return false;
+// }
+
+// DELTE
+
+
+
--- src/JS/models/functions.js
+++ src/JS/models/functions.js
@@ -2,10 +2,25 @@ import { objects } from "./objects.js";
 import { chart } from "../views/chart.js";
 
 
+export function init(){
+//   let options = '';
+// options += '<option value="test" />';
+// objects.historyList.innerHTML = '<option value="Now" />';
+// readCookie();
+let cookieList = readCookie()
+cookieList.forEach(el  => {
+if(el[0]){
+// console.log(el[0])
+  objects.historyList.innerHTML += `<option value=${el[0]} />`;
+}
+})
+return cookieList
+}
 export function totalValue(){
     let totalVal = 0;
     [].forEach.call(objects.itemVal, (el) => {
       totalVal += Number(el.innerHTML);
+      // console.log(totalVal)
     });
     objects.total.innerHTML = `${totalVal}`;
 }
@@ -13,6 +28,7 @@ export function totalValue(){
 export function cleanInput(){
   inputDescr.value = ""
   inputVal.value = ""
+  objects.inputDescr.focus();
 }
 
 export function addData(chart, label, data, id) {
@@ -42,4 +58,92 @@ export function randomColors(){
       chart.data.datasets[0].backgroundColor[i] = color + "66";
       chart.data.datasets[0].borderColor[i] = color;
     }
+}
+
+export function storeData(list, date, expireTimeInDays){
+  if (navigator.cookieEnabled) { 
+    
+    const cookieName = encodeURIComponent(date);
+    const cookieVal = encodeURIComponent(list);
+    let cookieText = cookieName + "=" + cookieVal;
+
+    if (typeof expireTimeInDays === "number") {
+        const data = new Date();
+        data.setTime(data.getTime() + (expireTimeInDays * 24*60*60*1000));
+        cookieText += "; expires=" + data.toGMTString();
+    }
+
+    // if (path) {
+    //     cookieText += "; path=" + path;
+    // }
+    // if (domain) {
+    //     cookieText += "; domain=" + domain;
+    // }
+    // if (secure) {
+    //     cookieText += "; secure";
+    // }
+    // console.log(cookieName)
+    // console.log(list)
+    // console.log(cookieText)
+
+    document.cookie = cookieText;
+  }
+}
+export function readCookie() {
+  let cookieName,cookieValues;
+  let cookieList = [];
+  // let j = 0;
+  const cookie = document.cookie.split(/; */);
+  cookie.forEach(el => {
+            const cookiePart = el.split("=");
+             cookieName = decodeURIComponent(cookiePart[0]);
+             cookieValues = decodeURIComponent(cookiePart[1]);
+             cookieValues = cookieValues.split(",")
+            //  cookieValues2Darr = [];
+            //  cookieList[j] = cookieList[cookieName,[cookieValues[0],cookieValues[1]]]
+            //  cookieList.push([cookieName,[cookieValues[0],cookieValues[1]]])
+let a = []
+           for (let i=0; i < (Math.round((cookieValues.length/2))); i++) {
+               a[i] = new Array(2)
+               a[i][0] = cookieValues[i*2]
+               a[i][1] = cookieValues[(i*2)+1]
+         }
+             cookieList.push([cookieName,[a]])
+  }) 
+  // console.log(cookieList);
+
+  return cookieList
+}
+
+// function deleteCookie(name) {
+//   const data = new Date();
+//   data.setTime(date.getMonth()-1);
+//   document.cookie = encodeURIComponent(name) + "=; expires=" + data.toGMTString();
+//   // document.cookie = encodeURIComponent(name) + "=; expires=" + data.toGMTString();
+// }
+export function deleteCookie(name) {
+  const cookieName = encodeURIComponent(name);
+  document.cookie = cookieName + "=; max-age=-1;";
+  // console.log(`delete: ${name}`)
+}
+
+export function clearData(items,chart){
+  let presentItems = items.length;
+  if(presentItems){
+    for(let i=0;i<presentItems;i++){
+      items[0].remove();
+    }
+    chart.data.labels.splice(0, presentItems);
+    chart.data.datasets[0].data.splice(0, presentItems);
+    chart.update();
+  //     let presentItems = objects.items.length;
+  // if(presentItems){
+  //   for(let i=0;i<presentItems;i++){
+  //     objects.item[0].remove();
+  //   }
+  //   chart.data.labels.splice(0, presentItems);
+  //   chart.data.datasets[0].data.splice(0, presentItems);
+  //   chart.update();
+  
+  }
 }
